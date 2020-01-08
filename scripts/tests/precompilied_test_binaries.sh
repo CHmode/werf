76d1884cdc62a9996b814a7fcdf883b23d746d98
Alexey Igrychev <alexey.igrychev@flant.com>
2020-01-08 22:48:04 +0000

    +

--- scripts/tests/precompilied_test_binaries.sh
+++ scripts/tests/precompilied_test_binaries.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+project_dir=$script_dir/../..
 
 find_dir=${1:-.}
-binaries_dir=${2:-precompiled_test_binaries}
-go_test_extra_args="${@:3}"
+test_binaries_output_dirname=${2:-$project_dir/precompiled_test_binaries}
+go_test_extra_tags="${*:3}"
 
-packages=$(find $find_dir -type f -name '*_test.go' -printf '%h\n' | sort -u)
-for package in $packages; do
-	filename=$(echo $package | tr / _).test;
-	filename=${filename:2}
-	go test $go_test_extra_args $package -coverpkg=./... -c -o $binaries_dir/$filename
+package_paths=$(find "$find_dir" -type f -name '*_test.go' -printf '%h\n' | sort -u)
+for package_path in $package_paths; do
+	test_binary_filename=$(echo "$package_path" | tr / _).test
+	test_binary_filename=${test_binary_filename:2}
+	go test --tags "dfrunmount dfssh $go_test_extra_tags" "$package_path" -coverpkg=./... -c -o "$test_binaries_output_dirname"/"$test_binary_filename"
 done
