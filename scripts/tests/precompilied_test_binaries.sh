4a9b87a99ac0547abd7feff26c583323c408eee0
Alexey Igrychev <alexey.igrychev@flant.com>
2020-01-08 23:04:34 +0000

    +

--- scripts/tests/precompilied_test_binaries.sh
+++ scripts/tests/precompilied_test_binaries.sh
@@ -7,7 +7,14 @@ find_dir=${1:-.}
 test_binaries_output_dirname=${2:-$project_dir/precompiled_test_binaries}
 go_test_extra_tags="${*:3}"
 
-package_paths=$(find "$find_dir" -type f -name '*_test.go' -printf '%h\n' | sort -u)
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install findutils
+  package_paths=$(gfind "$find_dir" -type f -name '*_test.go' -printf '%h\n' | sort -u)
+else
+  package_paths=$(find "$find_dir" -type f -name '*_test.go' -printf '%h\n' | sort -u)
+fi
+
 for package_path in $package_paths; do
 	test_binary_filename=$(echo "$package_path" | tr / _).test
 	test_binary_filename=${test_binary_filename:2}
